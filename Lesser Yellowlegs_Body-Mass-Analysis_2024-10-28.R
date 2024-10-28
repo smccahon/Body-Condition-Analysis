ba4383c48aca238d209a9878277ca4644a4ebfd5
Shelby McCahon <smccahon2@alaska.edu>
2024-10-28 12:10:43 -0700

    Created new repository for body condition analysis

---
+++ Lesser Yellowlegs_Body-Mass-Analysis_2024-10-28.R
@@ -0,0 +1,17 @@
+#--------------------------------------#
+# Lesser Yellowlegs Body Mass Analysis #
+#         Created 10/28/2024           #
+#         Modified 10/28/2024          #
+#--------------------------------------#
+
+# load packages
+library(dplyr)
+library(ggplot2)
+library(AICcmodavg)
+library(RColorBrewer)
+library(trtools)
+library(tidyverse)
+
+# load data
+getwd()
+
